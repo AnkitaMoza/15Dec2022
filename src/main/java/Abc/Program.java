a2c0c8d3e4b2db817a77d2c6e53f0c7c49efd4be
Ankita Mozar <ankita.mozar@gmail.com>
2022-12-16 11:11:34 +0530

    i have created String program .please verify it

---
+++ src/main/java/Abc/Program.java
@@ -0,0 +1,14 @@
+package Abc;
+
+public class Program {
+ public static void main(String[] args) {
+	
+	String name = "Ankita Mozar";
+	
+	boolean a =name.isBlank();
+	
+	System.out.println(a);
+	 
+	 
+}
+}
